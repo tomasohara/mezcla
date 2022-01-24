77b36f13540ba814dc48365042d88f8d9f3b32d7
Tom O'Hara <tomasohara@gmail.com>
2022-01-23 18:14:10 -0600

    adds test_debug_val() and fleshes out test_debug_code()

--- mezcla/tests/test_debug.py
+++ mezcla/tests/test_debug.py
@@ -78,11 +78,34 @@ class TestDebug:
         assert(captured.err == self.expected_stderr_trace)
         debug.trace_expr(6, pre_captured, captured)
 
-    def test_debug_code(self, capsys):
-        """Make sure debug code not executed at all"""
-        ## TODO: debug.assertion(debug_level, debug.code(debug_level, lambda: (8 / 0 != 0.0)))
-        assert(False)
+    def test_debug_val(self):
+        """Make sure debug.val only returns value when at specified level"""
+        debug.trace(4, f"test_debug_val(): self={self}")
+        save_trace_level = debug.get_level()
+        test_value = 22
+        debug.set_level(5)
+        level5_value = debug.val(5, test_value)
+        debug.set_level(0)
+        level0_value = debug.val(1, test_value)
+        debug.set_level(save_trace_level)
+        assert(level5_value == test_value)
+        assert(level0_value is None)
         
+    def test_debug_code(self):
+        """Make sure debug code not executed at all"""
+        debug.trace(4, f"test_debug_value(): self={self}")
+        ## TODO: debug.assertion(debug_level, debug.code(debug_level, lambda: (8 / 0 != 0.0)))
+        save_trace_level = debug.get_level()
+        count = 0
+        def increment():
+            """Increase counter"""
+            nonlocal count
+            count += 1
+        debug.set_level(4)
+        debug.code(4, lambda: increment)
+        debug.set_level(save_trace_level)
+        assert(count == 0)
+
 #------------------------------------------------------------------------
 
 if __name__ == '__main__':
