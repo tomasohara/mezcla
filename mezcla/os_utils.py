b677ff82147180b83b33431d03a5f8d72b83cc10
Tom O'Hara <tomasohara@gmail.com>
2022-03-23 01:00:34 -0500

    initial version--no-op for now

---
+++ mezcla/os_utils.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+#
+# Functions for operating system related access, such as running command or
+# getting environment values.
+#
+# TODO:
+# - Move functions from system.py here.
+#
