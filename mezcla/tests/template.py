7904d43692b4bb414616277966796b6786c5e47c
Tom O'Hara <tomasohara@gmail.com>
2022-04-11 23:06:36 -0500

    reworks cut-n-paste template to use <module>; adds note about ALLOW_SUBCOMMAND_TRACING; add env. var template; uses xyz.pdq-compatible get_testing_module_name

--- mezcla/tests/template.py
+++ mezcla/tests/template.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python
 #
-# TODO: Test(s) for ../MODULE.py
+# TODO: Test(s) for ../<module>.py
 #
 # Notes:
 # - Fill out TODO's below. Use numbered tests to order (e.g., test_1_usage).
 # - TODO: If any of the setup/cleanup methods defined, make sure to invoke base
 #   (see examples below for setUp and tearDown).
+# - For debugging the tested script, the ALLOW_SUBCOMMAND_TRACING environment
+#   option shows tracing output normally suppressed by  unittest_wrapper.py.
 # - This can be run as follows:
-#   $ PYTHONPATH=".:$PYTHONPATH" python tests/test_MODULE.py
+#   $ PYTHONPATH="$(realpath .)/..):$PYTHONPATH" python tests/test_<module>.py
 #
 
-"""TODO: Tests for TODO:module module"""
+"""TODO: Tests for <module> module"""
 
 # Standard packages
 import re
@@ -22,18 +24,32 @@ import unittest
 # Local packages
 from mezcla.unittest_wrapper import TestWrapper
 from mezcla import debug
+## TODO: from mezcla import system
 from mezcla import glue_helpers as gh
 
 # Note: Two references are used for the module to be tested:
 #    THE_MODULE:	    global module object
 #    TestIt.script_module   string name
 ## TODO: template => new name
-import mezcla.template as THE_MODULE
+THE_MODULE = None                       ## TODO: remove
+## TODO: import mezcla.<module> as THE_MODULE
+#
+# Note: sanity test for customization (TODO: remove if desired)
+if not re.search(__file__, r"\btemplate.py$"):
+    debug.assertion("mezcla.template" not in str(THE_MODULE))
+
+## TODO:
+## # Environment options
+## # Note: These are just intended for internal options, not for end users.
+## # It also allows for enabling options in one place.
+## #
+## FUBAR = system.getenv_bool("FUBAR", False,
+##                            description="Fouled Up Beyond All Recognition processing")
 
 
 class TestIt(TestWrapper):
     """Class for testcase definition"""
-    script_module = TestWrapper.derive_tested_module_name(__file__)
+    script_module = TestWrapper.get_testing_module_name(__file__)
     # TODO: use_temp_base_dir = True            # treat TEMP_BASE as directory
     # note: temp_file defined by parent (along with script_module, temp_base, and test_num)
 
