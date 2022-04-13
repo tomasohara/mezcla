7eb6958b18973a2d70b4c2447f7db96bfa9b1e31
Tom O'Hara <tomasohara@gmail.com>
2022-04-13 17:57:51 -0500

    fix for missing imports

--- mezcla/plot_utils.py
+++ mezcla/plot_utils.py
@@ -5,8 +5,20 @@
 
 """Plotting utility functions"""
 
+# Standard modules
+# TODO
+
+# Installed modules
+import matplotlib.pyplot as plt
+
+# Local modules
+from mezcla import debug
+from mezcla import system
+
+
 def reset_plot():
     """Reset the mathplotlib state"""
+    debug.trace(4, "reset_plot()")
     # Note: See https://stackoverflow.com/questions/741877/how-do-i-tell-matplotlib-that-i-am-done-with-a-plot
     # TODO: Make sure no other state retained
 
@@ -16,6 +28,8 @@ def reset_plot():
     # Clear the figure
     plt.clf()
     return
+
+#-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     system.print_error("Warning: Not intended for direct invocation.")
