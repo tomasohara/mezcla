ba8536bae561c51bfb77799e3a33b18155db7189
LimaBD <bdl1998@hotmail.com>
2022-08-03 21:15:02 -0300

    added tests for transitive_closure() is_prime() fibonacci() unzip() eval_expression() exactly1() string_diff()

--- mezcla/tests/test_misc_utils.py
+++ mezcla/tests/test_misc_utils.py
@@ -12,16 +12,12 @@
 """Tests for misc_utils module"""
 
 # Standard packages
-import re
-import os
 
 # Installed packages
 import pytest
 
 # Local packages
 from mezcla import debug
-from mezcla import glue_helpers as gh
-from mezcla import system
 
 # Note: Two references are used for the module to be tested:
 #    THE_MODULE:	    global module object
@@ -34,7 +30,10 @@ class TestMiscUtils:
     def test_transitive_closure(self):
         """Ensure transitive_closure works as expected"""
         debug.trace(4, "test_transitive_closure()")
-        ## TODO: WORK-IN-PROGRESS
+
+        actual = THE_MODULE.transitive_closure([(1, 2), (2, 3), (3, 4)])
+        expected = set([(1, 2), (1, 3), (1, 4), (2, 3), (3, 4), (2, 4)])
+        assert actual == expected
 
     def test_read_tabular_data(self):
         """Ensure read_tabular_data works as expected"""
@@ -44,17 +43,38 @@ class TestMiscUtils:
     def test_extract_string_list(self):
         """Ensure extract_string_list works as expected"""
         debug.trace(4, "test_extract_string_list()")
-        ## TODO: WORK-IN-PROGRESS
+        ## TODO:
+        ## - WORK-IN-PROGRESS
+        ## - Check switch method should be the main tested, if this or 
+        ##   text_utils.extract_string_list()
 
     def test_is_prime(self):
         """Ensure is_prime works as expected"""
         debug.trace(4, "test_is_prime()")
-        ## TODO: WORK-IN-PROGRESS
+
+        FIRST_100_PRIMES = [
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+            41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+            89, 97, 101, 103, 107, 109, 113, 127, 131,
+            137, 139, 149, 151, 157, 163, 167, 173, 179,
+            181, 191, 193, 197, 199, 211, 223, 227, 229,
+            233, 239, 241, 251, 257, 263, 269, 271, 277,
+            281, 283, 293, 307, 311, 313, 317, 331, 337,
+            347, 349, 353, 359, 367, 373, 379, 383, 389,
+            397, 401, 409, 419, 421, 431, 433, 439, 443,
+            449, 457, 461, 463, 467, 479, 487, 491, 499,
+            503, 509, 521, 523, 541
+        ]
+
+        assert all([THE_MODULE.is_prime(n) for n in FIRST_100_PRIMES])
+        assert all([(not THE_MODULE.is_prime(n)) for n in range(FIRST_100_PRIMES[-1])  if n not in FIRST_100_PRIMES])
 
     def test_fibonacci(self):
         """Ensure fibonacci works as expected"""
         debug.trace(4, "test_fibonacci()")
-        ## TODO: WORK-IN-PROGRESS
+        assert THE_MODULE.fibonacci(-5) == []
+        assert THE_MODULE.fibonacci(1) == [0]
+        assert THE_MODULE.fibonacci(10) == [0, 1, 1, 2, 3, 5, 8]
 
     def test_sort_weighted_hash(self):
         """Ensure sort_weighted_hash works as expected"""
@@ -64,7 +84,10 @@ class TestMiscUtils:
     def test_unzip(self):
         """Ensure unzip works as expected"""
         debug.trace(4, "test_unzip()")
-        ## TODO: WORK-IN-PROGRESS
+        assert THE_MODULE.unzip(zip([1, 2, 3], ['a', 'b', 'c'])) == [[1, 2, 3], ['a', 'b', 'c']]
+        assert THE_MODULE.unzip(zip([], []), 2) == [[], []]
+        assert THE_MODULE.unzip(zip(), 4) == [[], [], [], []]
+        assert THE_MODULE.unzip(zip(), 4) != [[], []]
 
     def test_get_current_frame(self):
         """Ensure get_current_frame works as expected"""
@@ -74,7 +97,7 @@ class TestMiscUtils:
     def test_eval_expression(self):
         """Ensure eval_expression works as expected"""
         debug.trace(4, "test_eval_expression()")
-        ## TODO: WORK-IN-PROGRESS
+        assert THE_MODULE.eval_expression("len([123, 321]) == 2")
 
     def test_trace_named_object(self):
         """Ensure trace_named_object works as expected"""
@@ -89,12 +112,30 @@ class TestMiscUtils:
     def test_exactly1(self):
         """Ensure exactly1 works as expected"""
         debug.trace(4, "test_exactly1()")
-        ## TODO: WORK-IN-PROGRESS
+        assert THE_MODULE.exactly1([False, False, True])
+        assert not THE_MODULE.exactly1([False, True, True])
+        assert not THE_MODULE.exactly1([False, False, False])
+        assert not THE_MODULE.exactly1([])
 
     def test_string_diff(self):
         """Ensure string_diff works as expected"""
         debug.trace(4, "test_string_diff()")
-        ## TODO: WORK-IN-PROGRESS
+
+        STRING_ONE = 'one\ntwo\nthree\nfour'
+        STRING_TWO = 'one\ntoo\ntree\nfour'
+        EXPECTED_DIFF = (
+            '  one\n'
+            '< two\n'
+            '…  ^\n'
+            '> too\n'
+            '…  ^\n'
+            '< three\n'
+            '…  -\n'
+            '> tree\n'
+            '  four\n'
+        )
+
+        assert THE_MODULE.string_diff(STRING_ONE, STRING_TWO) == EXPECTED_DIFF
 
     def test_elide_string_values(self):
         """Ensure elide_string_values works as expected"""
