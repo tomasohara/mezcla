416461b7a187be710ebb54f3042f759559a1bb36
LimaBD <bdl1998@hotmail.com>
2022-08-14 01:23:41 -0300

    added tests file for run_albert_classifier module

---
+++ mezcla/tests/test_run_albert_classifier.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#
+# Test(s) for ../run_albert_classifier.py
+#
+# Notes:
+# - For debugging the tested script, the ALLOW_SUBCOMMAND_TRACING environment
+#   option shows tracing output normally suppressed by  unittest_wrapper.py.
+# - This can be run as follows:
+#   $ PYTHONPATH="$(realpath .)/..):$PYTHONPATH" python tests/test_run_albert_classifier.py
+#
+
+"""Tests for run_albert_classifier module"""
+
+# Standard packages
+
+# Installed packages
+import pytest
+
+# Local packages
+from mezcla import debug
+
+# Note: Two references are used for the module to be tested:
+#    THE_MODULE:	    global module object
+#    TestIt.script_module   string name
+import mezcla.run_albert_classifier as THE_MODULE
+
+class TestIt:
+    """Class for testcase definition"""
+
+    ## TODO: TESTS WORK-IN-PROGRESS
+
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
