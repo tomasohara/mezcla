d4d3c9d60c5aedeaac3ada06369c4b608c80e901
LimaBD <bdl1998@hotmail.com>
2022-08-14 01:25:42 -0300

    added tests file for merge_notes module

---
+++ mezcla/tests/test_merge_notes.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#
+# Test(s) for ../merge_notes.py
+#
+# Notes:
+# - For debugging the tested script, the ALLOW_SUBCOMMAND_TRACING environment
+#   option shows tracing output normally suppressed by  unittest_wrapper.py.
+# - This can be run as follows:
+#   $ PYTHONPATH="$(realpath .)/..):$PYTHONPATH" python tests/test_merge_notes.py
+#
+
+"""Tests for merge_notes module"""
+
+# Standard packages
+
+# Installed packages
+import pytest
+
+# Local packages
+from mezcla import debug
+
+# Note: Two references are used for the module to be tested:
+#    THE_MODULE:	    global module object
+#    TestIt.script_module   string name
+import mezcla.merge_notes as THE_MODULE
+
+class TestIt:
+    """Class for testcase definition"""
+
+    ## TODO: TESTS WORK-IN-PROGRESS
+
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
