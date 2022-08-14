873eae8614481c939acb8f7bde1df214c58de36c
LimaBD <bdl1998@hotmail.com>
2022-08-14 01:20:49 -0300

    added tests file for text_processing module

---
+++ mezcla/tests/test_text_processing.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#
+# Test(s) for ../text_processing.py
+#
+# Notes:
+# - For debugging the tested script, the ALLOW_SUBCOMMAND_TRACING environment
+#   option shows tracing output normally suppressed by  unittest_wrapper.py.
+# - This can be run as follows:
+#   $ PYTHONPATH="$(realpath .)/..):$PYTHONPATH" python tests/test_text_processing.py
+#
+
+"""Tests for text_processing module"""
+
+# Standard packages
+
+# Installed packages
+import pytest
+
+# Local packages
+from mezcla import debug
+
+# Note: Two references are used for the module to be tested:
+#    THE_MODULE:	    global module object
+#    TestIt.script_module   string name
+import mezcla.text_processing as THE_MODULE
+
+class TestIt:
+    """Class for testcase definition"""
+
+    ## TODO: TESTS WORK-IN-PROGRESS
+
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
