e5a7ff6428ebf9a1e1c991338ccaa9f1d0f9f83a
LimaBD <bdl1998@hotmail.com>
2022-08-17 21:29:07 -0300

    added missing tests for html methods

--- mezcla/tests/test_html_utils.py
+++ mezcla/tests/test_html_utils.py
@@ -45,6 +45,10 @@ class TestIt(TestWrapper):
     # TODO: use_temp_base_dir = True            # treat TEMP_BASE as directory
     # note: temp_file defined by parent (along with script_module, temp_base, and test_num)
 
+    def test_get_browser(self):
+        """Ensure get_browser() works as expected"""
+        debug.trace(4, "test_get_browser()")
+        ## TODO: WORK-IN-PROGRESS
 
     def test_get_url_parameter_value(self):
         """Ensure get_url_parameter_value works as expected"""
@@ -96,7 +100,122 @@ class TestIt(TestWrapper):
         debug.trace_expr(5, rendered_html)
         self.assertTrue(re.search(r"<li>Browser dimensions:\s*<span.*>\d+x\d+</span></li>",
                                   rendered_html))
-    
+
+    def test_document_ready(self):
+        """Ensure document_ready() works as expected"""
+        debug.trace(4, "test_document_ready()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_escape_html_value(self):
+        """Ensure escape_html_value() works as expected"""
+        debug.trace(4, "test_escape_html_value()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_unescape_html_value(self):
+        """Ensure unescape_html_value() works as expected"""
+        debug.trace(4, "test_unescape_html_value()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_escape_hash_value(self):
+        """Ensure escape_hash_value() works as expected"""
+        debug.trace(4, "test_escape_hash_value()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_get_param_dict(self):
+        """Ensure get_param_dict() works as expected"""
+        debug.trace(4, "test_get_param_dict()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_set_param_dict(self):
+        """Ensure set_param_dict() works as expected"""
+        debug.trace(4, "test_set_param_dict()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_get_url_param(self):
+        """Ensure get_url_param() works as expected"""
+        debug.trace(4, "test_get_url_param()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_get_url_param_checkbox_spec(self):
+        """Ensure get_url_param_checkbox_spec() works as expected"""
+        debug.trace(4, "test_get_url_param_checkbox_spec()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_get_url_parameter_bool(self):
+        """Ensure get_url_parameter_bool() works as expected"""
+        debug.trace(4, "test_get_url_parameter_bool()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_get_url_parameter_int(self):
+        """Ensure get_url_parameter_int() works as expected"""
+        debug.trace(4, "test_get_url_parameter_int()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_fix_url_parameters(self):
+        """Ensure fix_url_parameters() works as expected"""
+        debug.trace(4, "test_fix_url_parameters()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_expand_misc_param(self):
+        """Ensure expand_misc_param() works as expected"""
+        debug.trace(4, "test_expand_misc_param()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test__read_file(self):
+        """Ensure _read_file() works as expected"""
+        debug.trace(4, "test__read_file()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test__write_file(self):
+        """Ensure _write_file() works as expected"""
+        debug.trace(4, "test__write_file()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_old_download_web_document(self):
+        """Ensure old_download_web_document() works as expected"""
+        debug.trace(4, "test_old_download_web_document()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_download_web_document(self):
+        """Ensure download_web_document() works as expected"""
+        debug.trace(4, "test_download_web_document()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_test_download_html_document(self):
+        """Ensure test_download_html_document() works as expected"""
+        debug.trace(4, "test_test_download_html_document()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_download_html_document(self):
+        """Ensure download_html_document() works as expected"""
+        debug.trace(4, "test_download_html_document()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_download_binary_file(self):
+        """Ensure download_binary_file() works as expected"""
+        debug.trace(4, "test_download_binary_file()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_retrieve_web_document(self):
+        """Ensure retrieve_web_document() works as expected"""
+        debug.trace(4, "test_retrieve_web_document()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_init_BeautifulSoup(self):
+        """Ensure init_BeautifulSoup() works as expected"""
+        debug.trace(4, "test_init_BeautifulSoup()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_extract_html_link(self):
+        """Ensure extract_html_link() works as expected"""
+        debug.trace(4, "test_extract_html_link()")
+        ## TODO: WORK-IN-PROGRESS
+
+    def test_extract_html_link(self):
+        """Ensure extract_html_link() works as expected"""
+        debug.trace(4, "test_extract_html_link()")
+        ## TODO: WORK-IN-PROGRESS
+
 #------------------------------------------------------------------------
 
 if __name__ == '__main__':
