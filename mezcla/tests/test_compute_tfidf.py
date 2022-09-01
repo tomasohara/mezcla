d3c98dcca6c7c52c59b65b4fe7ca8f2fecb0ddc9
LimaBD <bdl1998@hotmail.com>
2022-09-01 15:29:26 -0300

    added tests for get_suffix1_prefix2, terms_overlap, is_subsumed and work-in-progress tests

--- mezcla/tests/test_compute_tfidf.py
+++ mezcla/tests/test_compute_tfidf.py
@@ -27,8 +27,30 @@ import mezcla.compute_tfidf as THE_MODULE
 class TestIt:
     """Class for testcase definition"""
 
-    ## TODO: TESTS WORK-IN-PROGRESS
+    def test_show_usage_and_quit(self):
+        """Ensure show_usage_and_quit works as expected"""
+        debug.trace(4, "test_show_usage_and_quit()")
+        ## TODO: WORK-IN=PROGRESS
 
+    def test_get_suffix1_prefix2(self):
+        """Ensure get_suffix1_prefix2 works as expected"""
+        debug.trace(4, "test_get_suffix1_prefix2()")
+        assert THE_MODULE.get_suffix1_prefix2(["my", "dog"], ["dog", "has"]) == ["dog"]
+        assert THE_MODULE.get_suffix1_prefix2(["a", "b", "c"], ["b", "d"]) == []
+
+    def test_terms_overlap(self):
+        """Ensure terms_overlap works as expected"""
+        debug.trace(4, "test_terms_overlap()")
+        assert THE_MODULE.terms_overlap("ACME Rocket Research", "Rocket Research Labs") == "Rocket Research"
+        assert not THE_MODULE.terms_overlap("Rocket Res", "Rocket Research")
+
+    def test_is_subsumed(self):
+        """Ensure is_subsumed works as expected"""
+        debug.trace(4, "test_is_subsumed()")
+        assert THE_MODULE.is_subsumed("White House", ["The White House", "Congress", "Supreme Court"])
+        assert THE_MODULE.is_subsumed("White House", ["White Houses"])
+
+    ## TODO: Add tests for main()
 
 if __name__ == '__main__':
     debug.trace_current_context()
