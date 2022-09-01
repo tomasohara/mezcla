2ca9f87886e4ae1fe2c9288c5e061838791d534f
LimaBD <bdl1998@hotmail.com>
2022-09-01 15:06:17 -0300

    standarized module being tested to THE_MODULE

--- mezcla/tests/test_tpo_common.py
+++ mezcla/tests/test_tpo_common.py
@@ -22,7 +22,11 @@ import pytest
 
 # Local packages
 from mezcla import debug
-from mezcla import tpo_common as tpo
+
+# Note: Two references are used for the module to be tested:
+#    THE_MODULE:	    global module object
+#    TestIt.script_module   string name
+import mezcla.tpo_common as THE_MODULE
 
 FUBAR = 101	# sample global for test_format
 FOOBAR = 12     # likewise
@@ -34,44 +38,44 @@ class TestIt(unittest.TestCase):
     def test_format(self):
         """Ensure format resolves from local and global namespace, and that local takes precedence"""
         fubar = 202
-        assert tpo.format("{F} vs. {f}", F=FUBAR, f=fubar) == ("%s vs. %s" % (FUBAR, fubar))
+        assert THE_MODULE.format("{F} vs. {f}", F=FUBAR, f=fubar) == ("%s vs. %s" % (FUBAR, fubar))
         # pylint: disable=redefined-outer-name
         FOOBAR = 21
-        assert tpo.format("{FOO}", FOO=FOOBAR) == str(FOOBAR)
-        # TODO: assert "Hey Jos\xc3\xa9" == tpo.format("Hey {j}", j=JOSE)
+        assert THE_MODULE.format("{FOO}", FOO=FOOBAR) == str(FOOBAR)
+        # TODO: assert "Hey Jos\xc3\xa9" == THE_MODULE.format("Hey {j}", j=JOSE)
         return
 
     def test_get_current_function_name(self):
         """Test(s) for get_current_function_name()"""
-        assert tpo.get_current_function_name() == "test_get_current_function_name"
+        assert THE_MODULE.get_current_function_name() == "test_get_current_function_name"
         return
 
     def test_getenv_functions(self):
         """Ensure that various getenv_xyz functions work as expected"""
-        assert tpo.getenv_integer("REALLY FUBAR", 123) == 123
-        assert tpo.getenv_number("REALLY FUBAR", 123) == 123.0
-        assert not isinstance(tpo.getenv_boolean("REALLY FUBAR?", None), bool)
-        assert isinstance(tpo.getenv_boolean("REALLY FUBAR?", False), bool)
-        assert not isinstance(tpo.getenv_text("REALLY FUBAR?", False), bool)
+        assert THE_MODULE.getenv_integer("REALLY FUBAR", 123) == 123
+        assert THE_MODULE.getenv_number("REALLY FUBAR", 123) == 123.0
+        assert not isinstance(THE_MODULE.getenv_boolean("REALLY FUBAR?", None), bool)
+        assert isinstance(THE_MODULE.getenv_boolean("REALLY FUBAR?", False), bool)
+        assert not isinstance(THE_MODULE.getenv_text("REALLY FUBAR?", False), bool)
         os.environ["FUBAR"] = "1"
-        assert tpo.getenv_text("FUBAR") == "1"
+        assert THE_MODULE.getenv_text("FUBAR") == "1"
         return
 
     def test_unicode_functions(self):
         """Esnure that normalize_unicode, encode_unicode, etc. work as expected"""
         UTF8_BOM = "\xEF\xBB\xBF"
-        assert tpo.ensure_unicode("ASCII") == u"ASCII"
-        assert tpo.normalize_unicode("ASCII") == "ASCII"
-        ## TODO: assert tpo.ensure_unicode(UTF8_BOM) == u'\ufeff'
-        assert tpo.normalize_unicode(UTF8_BOM) == UTF8_BOM
-        assert u"Jos\xe9" == tpo.ensure_unicode(JOSE)
-        ## TODO: assert "Jos\xc3\xa9", tpo.normalize_unicode(JOSE)
+        assert THE_MODULE.ensure_unicode("ASCII") == u"ASCII"
+        assert THE_MODULE.normalize_unicode("ASCII") == "ASCII"
+        ## TODO: assert THE_MODULE.ensure_unicode(UTF8_BOM) == u'\ufeff'
+        assert THE_MODULE.normalize_unicode(UTF8_BOM) == UTF8_BOM
+        assert u"Jos\xe9" == THE_MODULE.ensure_unicode(JOSE)
+        ## TODO: assert "Jos\xc3\xa9", THE_MODULE.normalize_unicode(JOSE)
         return
 
     def test_difference(self):
         """Ensures set difference works as expected"""
-        assert tpo.difference([1, 2, 3], [2]) == [1, 3]
-        assert tpo.difference([1, 1, 2, 2], [1]) == [2]
+        assert THE_MODULE.difference([1, 2, 3], [2]) == [1, 3]
+        assert THE_MODULE.difference([1, 1, 2, 2], [1]) == [2]
         return
 
 if __name__ == '__main__':
