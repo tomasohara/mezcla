81acc476438bad4512e0ac76eac02d4b658b6c29
LimaBD <bdl1998@hotmail.com>
2022-09-10 17:47:19 -0300

    test for train_text_categorizer first commit

---
+++ mezcla/tests/test_train_text_categorizer.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+#
+# Tests for train_text_categorizer module
+#
+
+"""Tests for train_text_categorizer module"""
+
+# Standard modules
+## NOTE: this is empty for now
+
+# Installed modules
+import pytest
+
+# Local modules
+from mezcla import debug
+
+# Note: Rreference are used for the module to be tested:
+#    THE_MODULE:	    global module object
+import mezcla.train_text_categorizer as THE_MODULE
+
+
+## TODO: TESTS WORK-IN-PROGRESS
+
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
