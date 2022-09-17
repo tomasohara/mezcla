dc461b3e3db63ed49fc632a5f40e7b9093e58cf7
LimaBD <bdl1998@hotmail.com>
2022-09-17 16:04:57 -0300

    added export of COVERAGE_RCFILE

--- tools/run_tests.bash
+++ tools/run_tests.bash
@@ -5,7 +5,8 @@
 #
 
 tools=$(dirname $(realpath -s $0))
-mezcla=$tools/../mezcla
+base=$tools/..
+mezcla=$base/mezcla
 tests=$mezcla/tests
 
 echo -e "Running tests on $tests\n"
@@ -13,6 +14,7 @@ echo -e "Running tests on $tests\n"
 pip uninstall mezcla &> /dev/null # Avoid conflicts with installed Mezcla
 
 export PYTHONPATH="$mezcla/:$PYTHONPATH"
+export COVERAGE_RCFILE="$base/.coveragerc"
 
 coverage run -m pytest $tests
 coverage html
