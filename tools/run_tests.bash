e95d01cdbd159e6b193e72a1d72aa499a160d2be
LimaBD <bdl1998@hotmail.com>
2023-04-12 17:23:03 -0300

    added flag to enable run tests with coverage

--- tools/run_tests.bash
+++ tools/run_tests.bash
@@ -3,6 +3,10 @@
 # Run unitests and pytest files
 # and generate coverage report
 #
+# Usage:
+# $ ./tools/run_tests.bash
+# $ ./tools/run_tests.bash --coverage
+#
 
 tools=$(dirname $(realpath -s $0))
 base=$tools/..
@@ -14,9 +18,15 @@ echo -e "Running tests on $tests\n"
 pip uninstall mezcla &> /dev/null # Avoid conflicts with installed Mezcla
 
 export PYTHONPATH="$mezcla/:$PYTHONPATH"
-export COVERAGE_RCFILE="$base/.coveragerc"
-export CHECK_COVERAGE='true'
 
-coverage run -m pytest $tests
-coverage combine
-coverage html
+# Run with coverage enabled
+if [ "$1" == "--coverage" ]; then
+    export COVERAGE_RCFILE="$base/.coveragerc"
+    export CHECK_COVERAGE='true'
+    coverage erase
+    coverage run -m pytest $tests
+    coverage combine
+    coverage html
+else
+    pytest $tests
+fi
