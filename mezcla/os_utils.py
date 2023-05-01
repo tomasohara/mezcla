a7ae04098dbaa9e0f0b3f91fd501b11f945012ba
Tom O'Hara <tohara@tohara.com>
2023-05-01 15:25:32 -0500

    adds split_extension

--- mezcla/os_utils.py
+++ mezcla/os_utils.py
@@ -6,3 +6,25 @@
 # TODO:
 # - Move functions from system.py here.
 #
+
+"""OS utilities: mainly wrappers around os package"""
+
+# Standard modules
+import os
+
+# Local modules
+from mezcla import debug
+from mezcla import system
+
+
+def split_extension(path):
+    """Returns basename and extension for PATH"""
+    result = os.path.splitext(path)
+    try:
+        filename_proper, ext = result
+        debug.assertion(system.remove_extension(path) == filename_proper)
+        debug.assertion(system.get_extension(path) == ext[1:])
+    except:
+        pass
+    debug.trace(5, f"split_extension({path}) => {result}")
+    return result
