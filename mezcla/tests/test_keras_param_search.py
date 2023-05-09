3f8602a6e52ec4b8f04efea4c6cbb535425185f9
Tom O'Hara <tohara@tohara.com>
2023-05-08 19:32:27 -0500

    makes module import conditional as keras not part of default distribution given tensorflow size

--- mezcla/tests/test_keras_param_search.py
+++ mezcla/tests/test_keras_param_search.py
@@ -12,6 +12,7 @@
 """Tests for keras_param_search module"""
 
 # Standard packages
+## TODO: import re
 
 # Installed packages
 import pytest
@@ -19,32 +20,44 @@ import pytest
 # Local packages
 from mezcla import debug
 
+# Load module conditionally because not part of default installation.
 # Note: Two references are used for the module to be tested:
 #    THE_MODULE:	    global module object
-import mezcla.keras_param_search as THE_MODULE
+try:
+    import mezcla.keras_param_search as THE_MODULE
+except:
+    THE_MODULE = None
+    debug.trace_exception(1, "keras_param_search import")
 
 class TestKerasParamSearch:
     """Class for testcase definition"""
 
+    @pytest.mark.skipif(not THE_MODULE, reason="Unable to load module")
     def test_round3(self):
         """Ensure round3 works as expected"""
-        debug.trace(4, "test_round3()")
-        ## TODO: WORK-IN=PROGRESS
+        debug.trace(4, f"test_round3(); self={self}")
+        assert THE_MODULE.round3(1.0001) == 1.000
+        assert THE_MODULE.round3(1.001) == 1.001
 
+    @pytest.mark.xfail
     def test_non_negative(self):
         """Ensure non_negative works as expected"""
-        debug.trace(4, "test_non_negative()")
+        debug.trace(4, f"test_non_negative(); self={self}")
         ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
+    @pytest.mark.xfail
     def test_create_feature_mapping(self):
         """Ensure create_feature_mapping works as expected"""
-        debug.trace(4, "test_create_feature_mapping()")
+        debug.trace(4, f"test_create_feature_mapping(); self={self}")
         assert THE_MODULE.create_feature_mapping(['c', 'b', 'b', 'a']) == {'c':0, 'b':1, 'a':2}
 
+    @pytest.mark.xfail
     def test_create_keras_model(self):
         """Ensure create_keras_model works as expected"""
-        debug.trace(4, "test_create_keras_model()")
+        debug.trace(4, f"test_create_keras_model(); self={self}")
         ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
     ## TODO: test MyKerasClassifier class
     ## TODO: test main
