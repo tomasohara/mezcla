be7fcaed37fda5bad6ae0ccfb2ae5be054b27ba9
Tom O'Hara <tohara@tohara.com>
2023-06-01 16:35:44 -0500

    to 1.3.7

--- mezcla/__init__.py
+++ mezcla/__init__.py
@@ -18,7 +18,7 @@ Adhoc usage:
 Tom O'Hara
 Feb 2022
 """
-__VERSION__ = '1.3.6'
+__VERSION__ = '1.3.7'
 __version__ = __VERSION__
 
 # Standard module(s)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 setup(name='Mezcla',
       packages=['mezcla'],
       module="mezcla",
-      version='1.3.6',
+      version='1.3.7',
       description-file="README.txt",
       dist-name="Mezcla",
       ## OLD: py_modules=PYTHON_MODULE_NAMES,
