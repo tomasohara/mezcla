3fe552751c8d916465bb984e6b779ebb13bb2abb
Tom O'Hara <tohara@tohara.com>
2023-07-06 18:04:25 -0500

    traces out installation path

--- mezcla/__main__.py
+++ mezcla/__main__.py
@@ -17,6 +17,7 @@
 # Installed modules
 import mezcla
 from mezcla import debug
+from mezcla import glue_helpers as gh
 from mezcla.my_regex import my_re
 from mezcla import system
 
@@ -25,7 +26,9 @@ TL = debug.TL
 
 if __name__ == '__main__':
     system.print_error(f"Warning: {__file__} is not intended as standalone.")
+    file_path = system.real_path(gh.dirname(__file__))
     debug.trace(TL.USUAL, f"Version: {mezcla.__VERSION__}")
+    debug.trace(TL.USUAL, f"Install path: {file_path}")
 
     # Derive module name
     # ex: /home/tomohara/python/Mezcla/mezcla/__main__.py => "mezcla"
