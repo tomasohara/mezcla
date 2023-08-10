5eeb4acfd15762983dd0871b91bf9e0bd7350ecf
Tom O'Hara <tohara@tohara.com>
2023-08-10 03:14:41 -0500

    cleanup using @pytest.mark.xfail...assert(False) in place of IOU

--- mezcla/tests/test_text_categorizer.py
+++ mezcla/tests/test_text_categorizer.py
@@ -41,25 +41,29 @@ if not re.search(__file__, r"\btemplate.py$"):
 class TestTextCategorizerUtils(TestWrapper):
     """Class for testcase definition"""
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_something_else(self):
         """TODO: flesh out test for something else"""
         debug.trace(4, "test_something_else()")
-        ## ex: assert THE_MODULE.TODO_function() == TODO_value
+        assert(False)
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_sklearn_report(self):
         """Ensure sklearn_report works as expected"""
         debug.trace(4, "test_sklearn_report()")
-        ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_create_tabular_file(self):
         """Ensure create_tabular_file works as expected"""
         debug.trace(4, "test_create_tabular_file()")
-        ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_read_categorization_data(self):
         """Ensure read_categorization_data works as expected"""
         debug.trace(4, "test_read_categorization_data()")
-        ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
     def test_int_if_whole(self):
         """Ensure int_if_whole works as expected"""
@@ -67,12 +71,13 @@ class TestTextCategorizerUtils(TestWrapper):
         assert THE_MODULE.int_if_whole(2.0) == 2
         assert THE_MODULE.int_if_whole(2.999) == 2.999
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_param_or_default(self):
         """Ensure param_or_default works as expected"""
         debug.trace(4, "test_param_or_default()")
-        ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
-    @pytest.mark.skip(reason="TODO: test not yet implemented")
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_data_file(self):
         """Makes sure TODO works as expected"""
         debug.trace(4, "TestTextCategorizer.test_data_file()")
@@ -82,15 +87,17 @@ class TestTextCategorizerUtils(TestWrapper):
         assert re.search(r"TODO-pattern", output.strip())
         return
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_format_index_html(self):
         """Ensure format_index_html works as expected"""
         debug.trace(4, "test_format_index_html()")
-        ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_start_web_controller(self):
         """Ensure start_web_controller works as expected"""
         debug.trace(4, "test_start_web_controller()")
-        ## TODO: WORK-IN=PROGRESS
+        assert(False)
 
 
 class TestTextCategorizerScript(TestWrapper):
@@ -101,6 +108,12 @@ class TestTextCategorizerScript(TestWrapper):
     # note: temp_file defined by parent (along with script_module, temp_base, and test_num)
 
     ## TODO: WORK-IN-PROGRESS TESTS
+    @pytest.mark.xfail                   # TODO: remove xfail
+    def test_something_test_anything(self):
+        """TODO: flesh out test for something, anything!"""
+        debug.trace(4, "test_something_test_anything()")
+        assert(False)
+
 
 
 #------------------------------------------------------------------------
