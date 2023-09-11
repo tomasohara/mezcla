cf937c5452e10fde1be0d4b2f4c9e8a637c53681
Tom O'Hara <tohara@tohara.com>
2023-09-10 22:57:43 -0500

    TODO example with @trap_exception

--- mezcla/tests/template.py
+++ mezcla/tests/template.py
@@ -23,6 +23,7 @@ import pytest
 
 # Local packages
 from mezcla.unittest_wrapper import TestWrapper
+## TODO: from mezcla.unittest_wrapper import @trap_exception
 from mezcla import debug
 ## TODO: from mezcla import glue_helpers as gh
 from mezcla.my_regex import my_re
@@ -87,6 +88,7 @@ class TestIt(TestWrapper):
         return
 
     @pytest.mark.xfail                   # TODO: remove xfail
+    ## TODO: @trap_exception
     def test_something_else(self):
         """TODO: flesh out test for something else"""
         debug.trace(4, f"TestIt.test_something_else(); self={self}")
