e21650a9d9ac9bac89ded9a9866aeb09c839312e
Tom O'Hara <tohara@tohara.com>
2023-12-10 21:18:19 -0600

    fleshes out test case

--- mezcla/tests/test_extract_document_text.py
+++ mezcla/tests/test_extract_document_text.py
@@ -18,20 +18,34 @@
 import pytest
 
 # Local packages
+from mezcla.unittest_wrapper import TestWrapper
 from mezcla import debug
+from mezcla import system
 
 # Note: Two references are used for the module to be tested:
 #    THE_MODULE:	    global module object
 import mezcla.extract_document_text as THE_MODULE
 
-class TestExtractDocumentText:
+class TestExtractDocumentText(TestWrapper):
     """Class for testcase definition"""
+    script_module = TestWrapper.get_testing_module_name(__file__, THE_MODULE)
     
+    @pytest.mark.xfail
     def test_document_to_text(self):
         """Ensure document_to_text() works as expected"""
         debug.trace(4, "test_document_to_text()")
-        ## TODO: WORK-IN-PROGRESS
-
+        data = [
+            "<HTML lang='en'>",
+            "  <BODY>",
+            "    <H3>Hey</H3>"
+            "  </BODY>",
+            "</HTML>",
+            ]
+        temp_file = self.temp_file + ".html"
+        system.write_lines(temp_file, data)
+        output = self.run_script(options="", env_options="STDOUT=1", data_file=temp_file)
+        self.do_assert("Hey" == output.strip())
+        return
 
 if __name__ == '__main__':
     debug.trace_current_context()
