f50fa4b99e15b5a2638bc9565ceb96494acad2ff
Tom O'Hara <tohara@tohara.com>
2023-12-26 16:30:46 -0600

    fix to add actual test

--- mezcla/tests/test_spacy_nlp.py
+++ mezcla/tests/test_spacy_nlp.py
@@ -8,6 +8,27 @@
 # - This can be run as follows:
 #   $ PYTHONPATH=".:$PYTHONPATH" python ./mezcla/tests/test_spacy_nlp.py
 #
+#...............................................................................
+# Output from sample test:
+#
+# $ spacy_nlp.py - <<<$'I came, I saw, I conquered. The food\nwas bland.'
+# text  is_oov  is_stop sentiment       is_sent_start
+# It    False   True    0.0     True
+# came  False   False   0.0     False
+# ,     False   False   0.0     False
+# it    False   True    0.0     False
+# saw   False   False   0.0     False
+# ,     False   False   0.0     False
+# it    False   True    0.0     False
+# conquered     False   False   0.0     False
+# .     False   False   0.0     False
+# text  is_oov  is_stop sentiment       is_sent_start
+# The   False   True    0.0     True
+# food  False   False   0.0     False
+# was   False   True    0.0     False
+# bland False   False   0.0     False
+# .	False	False	0.0	False
+#
 
 """Tests for spacy_nlp module"""
 
@@ -20,6 +41,11 @@ import pytest
 # Local packages
 from mezcla import debug
 from mezcla import glue_helpers as gh
+from mezcla.my_regex import my_re
+from mezcla import system
+from mezcla.unittest_wrapper import TestWrapper
+## DEBUG:
+from mezcla.unittest_wrapper import trap_exception
 
 # Note: Two references are used for the module to be tested:
 #    THE_MODULE:	    global module object
@@ -28,9 +54,6 @@ try:
 except:
     THE_MODULE = None
     debug.trace_exception(1, "mezcla.spacy_nlp import")
-
-from mezcla.unittest_wrapper import TestWrapper
-
 
 class TestSentimentAnalyzer:
     """Class for testcase definition"""
@@ -72,6 +95,27 @@ class TestSpacyNlpUtils:
 class TestSpacy(TestWrapper):
     """Class for testcase definition"""
     script_module = TestWrapper.get_testing_module_name(__file__, THE_MODULE)
+
+    @pytest.mark.xfail                   # TODO: remove xfail
+    ## DEBUG:
+    @trap_exception            # TODO: remove when debugged
+    def test_data_file(self):
+        """Tests run_script w/ data file"""
+        debug.trace(4, f"TestIt.test_data_file(); self={self}")
+        data = ["It came, it saw, it conquered. The food", "was bland."]
+        system.write_lines(self.temp_file, data)
+        output = self.run_script(options="", data_file=self.temp_file)
+        actual = output.strip()
+        # Check for header
+        self.do_assert(my_re.search(r"text\tis_oov\tis_stop\tsentiment\tis_sent_start", actual))
+        # Check for start of sentences
+        self.do_assert(my_re.search(r"It\tFalse\tTrue\t0.0\tTrue", actual))
+        self.do_assert(my_re.search(r"The\tFalse\tTrue\t0.0\tTrue", actual))
+        # Check that no other sentence starts (n.b., "was" doesn't even though new line)
+        self.do_assert(my_re.search(r"was\tFalse\tTrue\t0.0\tFalse", actual))
+        sent_start_info = sorted(gh.extract_matches(r"\t(\S+)$", actual.splitlines()))
+        self.do_assert(sent_start_info[-5:] == ["False", "True", "True", "is_sent_start", "is_sent_start"])
+        return
 
     @pytest.mark.xfail
     ## TODO: @pytest.mark.skipif(not THE_MODULE, reason="Unable to load module")
