33fe93c174adc7ef2f8796b207c410a13a0b123c
Tom O'Hara <tohara@tohara.com>
2024-01-07 18:16:02 -0600

    fix to get tests operational; fix to properly follow test/template.py; decomposes test_document_to_text asserts; adds xfail to new tests and removes from html one; test cleanup

--- mezcla/tests/test_extract_document_text.py
+++ mezcla/tests/test_extract_document_text.py
@@ -12,13 +12,12 @@
 """Tests for extract_document_text module"""
 
 # Standard packages
-import re
+## OLD: import re
 
 # Installed packages
 import pytest
 
 # Local packages
-from mezcla.unittest_wrapper import TestWrapper
 from mezcla import debug
 from mezcla import glue_helpers as gh
 from mezcla import system
@@ -26,19 +25,23 @@ from mezcla.unittest_wrapper import TestWrapper
 
 
 # Note: Two references are used for the module to be tested:
-#    THE_MODULE:	    global module object
+#    THE_MODULE:                        global module object
+#    TestIt.script_module:              path to file
 import mezcla.extract_document_text as THE_MODULE
 
 class TestExtractDocumentText(TestWrapper):
     """Class for testcase definition"""
     script_module = TestWrapper.get_testing_module_name(__file__, THE_MODULE)
     
-    @pytest.mark.xfail
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_document_to_text(self):
         """Ensure document_to_text() works as expected"""
         debug.trace(4, "test_document_to_text()")
         # NOTE: Output contains \n specifier at the end on the text.
-        tmp_document = gh.get_temp_file()
+        # - Uses test-specific temp file as per TestWrapper.
+        # - Uses file extension for use by underlying textract module (see document_to_text).
+        ## OLD: tmp_document = gh.get_temp_file()
+        tmp_document = self.temp_file + ".txt"
 
         # DOCUMENT_TEXT = """Lorem Ipsum is simply dummy text of the printing and typesetting industry.
         # Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -50,29 +53,41 @@ class TestExtractDocumentText(TestWrapper):
         
         gh.write_file(tmp_document, text=DOCUMENT_TEXT)
         output = THE_MODULE.document_to_text(tmp_document)
-        output_case1 = output != DOCUMENT_TEXT
-        output_case2 = DOCUMENT_TEXT in output
-        output_case3 = output.endswith("\n")
-        output_case4 = output.strip() == DOCUMENT_TEXT
-        assert output_case1 and output_case2 and output_case3 and output_case4
+        ## OLD:
+        ## output_case1 = output != DOCUMENT_TEXT
+        ## output_case2 = DOCUMENT_TEXT in output
+        ## output_case3 = output.endswith("\n")
+        ## output_case4 = output.strip() == DOCUMENT_TEXT
+        ## assert output_case1 and output_case2 and output_case3 and output_case4
+        assert output != DOCUMENT_TEXT
+        assert DOCUMENT_TEXT in output
+        assert output.endswith("\n")
+        assert output.strip() == DOCUMENT_TEXT
         return
 
-    # TODO: Adding tests to check if extract_document_text.py works properly
+    @pytest.mark.xfail                   # TODO: remove xfail
     def test_document_to_text_IO(self):
         """Ensure document_to_text_IO() works as expected"""
+        # TODO: Adding tests to check if extract_document_text.py works properly
         debug.trace(4, "test_document_to_text_IO()")
-        env_option = "STDOUT"
+        ## OLD: env_option = "STDOUT"
         # NOTE: Output contains \n specifier at the end on the text.
-        doc_input_temp = gh.get_temp_file()
-        doc_output_temp = gh.get_temp_file()
+        ## OLD:
+        ## doc_input_temp = gh.get_temp_file()
+        ## doc_output_temp = gh.get_temp_file()
+        ## TODO2: carefully review tests/template.py
+        doc_input_temp = self.temp_file + ".txt"
         # Single-line string as INPUT
         DOCUMENT_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse porttitor rutrum leo. Nam efficitur justo quis mi ullamcorper consectetur. Aenean posuere nisl dignissim mauris blandit, sed euismod lacus posuere. Morbi ultrices magna eget faucibus ultricies. Praesent in ante nisi. Nulla faucibus sollicitudin sapien in aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus."
         gh.write_file(filename=doc_input_temp, text=DOCUMENT_TEXT)
-        test_command_1 = f"../extract_document_text.py {doc_input_temp} {doc_output_temp}"
-        gh.run(test_command_1)
-
-        output = gh.read_file(doc_output_temp)
-        assert output == DOCUMENT_TEXT
+        ## BAD:
+        ## test_command_1 = f"../extract_document_text.py {doc_input_temp} {doc_output_temp}"
+        ## gh.run(test_command_1)
+        ## output = gh.read_file(doc_output_temp)
+        output = self.run_script(env_options="STDOUT=1", data_file=doc_input_temp)
+        
+        ## BAD: assert output == DOCUMENT_TEXT
+        assert output.strip() == DOCUMENT_TEXT
         return 
         
     def test_document_to_text_html(self):
