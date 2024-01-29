d2d31f608decb03fb2d73df366716accfa98b31e
Tom O'Hara <tomasohara@gmail.com>
2024-01-28 20:53:35 -0600

    initial version

---
+++ mezcla/examples/tests/test_youtube_transcript.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+#
+# TODO: Test(s) for ../<module>.py
+#
+# Notes:
+# - Fill out TODO's below. Use numbered tests to order (e.g., test_1_usage).
+# - * See test_python_ast.py for simple example of customization.
+# - TODO: If any of the setup/cleanup methods defined, make sure to invoke base
+#   (see examples below for setUp and tearDown).
+# - For debugging the tested script, the ALLOW_SUBCOMMAND_TRACING environment
+#   option shows tracing output normally suppressed by unittest_wrapper.py.
+# - This can be run as follows:
+#   $ PYTHONPATH=".:$PYTHONPATH" python ./mezcla/tests/test_<module>.py
+#
+
+"""TODO: Tests for <module> module"""
+
+# Standard modules
+## TODO: from collections import defaultdict
+
+# Installed modules
+import pytest
+
+# Local modules
+from mezcla.unittest_wrapper import TestWrapper
+## TODO: from mezcla.unittest_wrapper import trap_exception
+from mezcla import debug
+## TODO: from mezcla import glue_helpers as gh
+from mezcla.my_regex import my_re
+from mezcla import system
+
+# Note: Two references are used for the module to be tested:
+#    THE_MODULE:                        global module object
+#    TestIt.script_module:              path to file
+## TODO (vvv): insert new module name in commented out template teo lines below
+import mezcla.examples.youtube_transcript as THE_MODULE
+#
+# Note: sanity test for customization (TODO: remove if desired)
+if not my_re.search(__file__, r"\btemplate.py$"):
+    debug.assertion("mezcla.template" not in str(THE_MODULE))
+
+## TODO:
+## # Environment options
+## # Note: These are just intended for internal options, not for end users.
+## # It also allows for enabling options in one place.
+## #
+## FUBAR = system.getenv_bool("FUBAR", False,
+##                            description="Fouled Up Beyond All Recognition processing")
+
+#------------------------------------------------------------------------
+
+class TestIt(TestWrapper):
+    """Class for command-line based testcase definition"""
+    script_module = TestWrapper.get_testing_module_name(__file__, THE_MODULE)
+    #
+    # TODO: use_temp_base_dir = True            # treat TEMP_BASE as directory
+    # note: temp_file defined by parent (along with script_module, temp_base, and test_num)
+    # TODO: use TestIt2 if capsys needed
+
+    @pytest.mark.xfail                   # TODO: remove xfail
+    ## DEBUG: @trap_exception            # TODO: remove when debugged
+    def test_data_file(self):
+        """Tests run_script w/ data file"""
+        debug.trace(4, f"TestIt.test_data_file(); self={self}")
+        data = ["TODO1", "TODO2"]
+        system.write_lines(self.temp_file, data)
+        ## TODO: add use_stdin=True to following if no file argument
+        output = self.run_script(options="", data_file=self.temp_file)
+        self.do_assert(my_re.search(r"TODO-pattern", output.strip()))
+        return
+
+    @pytest.mark.xfail                   # TODO: remove xfail
+    ## DEBUG: @trap_exception            # TODO: remove when debugged
+    def test_something_else(self):
+        """Test for something_else: TODO..."""
+        debug.trace(4, f"TestIt.test_something_else(); self={self}")
+        self.do_assert(False)
+        ## ex: self.do_assert(THE_MODULE.TODO_function() == TODO_value)
+        return
+
+    @pytest.mark.xfail                   # TODO: remove xfail
+    def test_whatever(self, capsys):
+        """TODO: flesh out test for whatever"""
+        debug.trace(4, f"TestIt2.test_whatever(); self={self}")
+        THE_MODULE.TODO_whatever()
+        captured = capsys.readouterr()
+        self.do_assert("whatever" in captured.err, "TODO_whatever trace")
+        return
+
+#------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
