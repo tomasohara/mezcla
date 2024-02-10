158954c4ed5c1874f9f53e4e590f1cd67455becb
Tom O'Hara <tohara@tohara.com>
2024-02-10 00:04:38 -0600

    shows filename and line# in assertion about trace_expr value resolution

--- mezcla/debug.py
+++ mezcla/debug.py
@@ -546,7 +546,7 @@ if __debug__:
                 if (match and match.group(1) in kwargs):
                     continue
                 assertion((not ((value is not None) and (expression is None))),
-                          "Warning: Likely problem resolving expression text (try reworking trace_expr call)")
+                          f"Warning: Likely problem resolving expression text (try reworking trace_expr call at {filename}:{line_number})")
                 value_spec = format_value(repr(value) if use_repr else value,
                                           max_len=max_len)
                 trace(level, f"{expression}={value_spec}{delim}", no_eol=no_eol)
