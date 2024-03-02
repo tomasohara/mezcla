8e94ebaf11dc3cdb7da315df4327c95f83370488
Tom O'Hara <tohara@tohara.com>
2024-03-02 16:11:16 -0600

    fixes test_03_gpu_usage amd refines test_01_index_dir; adds RUN_SLOW_TESTS; code cleanup

--- mezcla/tests/test_llm_desktop_search.py
+++ mezcla/tests/test_llm_desktop_search.py
@@ -17,7 +17,7 @@
 import pytest
 
 # Local modules
-from mezcla.unittest_wrapper import TestWrapper
+from mezcla.unittest_wrapper import TestWrapper, RUN_SLOW_TESTS
 ## TODO: from mezcla.unittest_wrapper import trap_exception
 from mezcla import debug
 from mezcla import glue_helpers as gh
@@ -28,18 +28,6 @@ from mezcla import system
 #    THE_MODULE:                        global module object
 #    TestIt.script_module:              path to file
 import mezcla.llm_desktop_search as THE_MODULE
-#
-# Note: sanity test for customization (TODO: remove if desired)
-if not my_re.search(__file__, r"\btemplate.py$"):
-    debug.assertion("mezcla.template" not in str(THE_MODULE))
-
-## TODO:
-## # Environment options
-## # Note: These are just intended for internal options, not for end users.
-## # It also allows for enabling options in one place.
-## #
-## FUBAR = system.getenv_bool("FUBAR", False,
-##                            description="Fouled Up Beyond All Recognition processing")
 
 #------------------------------------------------------------------------
 
@@ -50,34 +38,38 @@ class TestIt(TestWrapper):
     # TODO: use_temp_base_dir = True            # treat TEMP_BASE as directory
     # note: temp_file defined by parent (along with script_module, temp_base, and test_num)
 
+    @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="Ignoring slow test")
     @pytest.mark.xfail                   # TODO: remove xfail
     ## DEBUG: @trap_exception            # TODO: remove when debugged
     def test_01_index_dir(self):
         """Tests run_script to index directory"""
         debug.trace(4, f"TestIt.test_01_index_dir(); self={self}")
-        system.write_lines(self.temp_file, data)
         repo_base_dir = gh.form_path(gh.real_path(gh.dirname(__file__)),
                                      "..", "..")
         output = self.run_script(options=f"--index {repo_base_dir}")
+        self.do_assert(system.is_directory("faiss"))
         self.do_assert(my_re.search(r"TODO-pattern", output.strip()))
         return
 
+    @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="Ignoring slow test")
     @pytest.mark.xfail                   # TODO: remove xfail
     ## DEBUG: @trap_exception            # TODO: remove when debugged
     def test_02_search_docs(self):
         """Test for something_else: TODO..."""
         debug.trace(4, f"TestIt.test_02_search_docs(); self={self}")
-        output = self.run_script(options=f"--search 'What license is used?'")
+        output = self.run_script(options="--search 'What license is used?'")
         self.do_assert(my_re.search(r"GNU", output.strip()))
         return
 
     @pytest.mark.xfail                   # TODO: remove xfail
-    def test_02_whatever(self):
-        """TODO: flesh out test for whatever (capsys-like)"""
-        debug.trace(4, f"TestIt2.test_02_whatever(); self={self}")
-        THE_MODULE.TODO_whatever()
-        captured = self.get_stderr()
-        self.do_assert("whatever" in captured, "TODO_whatever trace")
+    def test_03_gpu_usage(self):
+        """Test for GPU libs being used"""
+        debug.trace(4, f"TestIt.test_03_gpu_usage(); self={self}")
+        ds = THE_MODULE.DesktopSearch()
+        ds.show_similar("license")
+        stdout, stderr = self.get_stdout_stderr()
+        self.do_assert("GNU" in stdout) 
+        self.do_assert("tensorflow" in stderr)
         return
 
 #------------------------------------------------------------------------
