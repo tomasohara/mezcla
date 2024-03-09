6fde4567d7679acf04ea149eade84c3e8516c670
Aviyan Acharya <avyn.xyz@gmail.com>
2024-03-09 17:51:00 +0545

    Added tests to "fleshed out" placeholder tests; Fixed debug.trace to "refined"

--- mezcla/tests/test_my_regex.py
+++ mezcla/tests/test_my_regex.py
@@ -37,65 +37,123 @@ class TestMyRegex(TestWrapper):
     ##     """Gets capsys"""
     ##     self.capsys = capsys
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_search(self):
         """Ensure search() works as expected"""
-        debug.trace(4, "test_search()")
-        assert(False)
+        debug.trace(4, f"test_search(); self={self}")
+        text = "The quick brown fox jumps over the lazy dog."
+        regex = "\w{5}"
+        self.my_re.search(regex, text)
+        output = self.my_re.get_match()
+        assert (output.group() == "quick" and output.span() == (4, 9))
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_match(self):
         """Ensure match() works as expected"""
-        debug.trace(4, "test_match()")
-        assert(False)
+        debug.trace(4, f"test_match(); self={self}")
+        text = "1 kiss is all takes."
+        regex = "\d+"
+        self.my_re.match(regex, text, 0)
+        output = self.my_re.get_match()
+        assert(output.group() == "1" and output.span() == (0, 1))
+        ## TODO: return the matched value (solved: use group() after my_re.get_match())
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_get_match(self):
         """Ensure get_match() works as expected"""
-        debug.trace(4, "test_get_match()")
-        assert(False)
+        debug.trace(4, f"test_get_match(); self={self}")
+        # get_match() returns the last the result of match
+        text = "333 little birds"
+        regex = "\d+"
+        self.my_re.match(regex, text, 0)
+        output = self.my_re.get_match()
+        assert isinstance(output, re.Match)
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_group(self):
         """Ensure group() works as expected"""
-        debug.trace(4, "test_group()")
-        assert(False)
+        debug.trace(4, f"test_group(); self={self}")
+        text = "three, 7, eight"
+        regex = "\w{5},"
+        self.my_re.search(regex, text, 0)
+        output = self.my_re.group(0)
+        assert (output == 'three,')
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_groups(self):
         """Ensure groups() works as expected"""
-        debug.trace(4, "test_groups()")
-        assert(False)
+        debug.trace(4, f"test_groups(); self={self}")
+        text = "John Doe: 30 years old, Jane Smith: 25 years old"
+        regex = "(\w+\s\w+): (\d+) years"
+        self.my_re.search(regex, text)
+        output = self.my_re.groups()
+        assert(output == ('John Doe', '30'))
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_grouping(self):
         """Ensure grouping() works as expected"""
-        debug.trace(4, "test_grouping()")
-        assert(False)
+        debug.trace(4, f"test_grouping(); self={self}")
+        text = "John Doe: 30 years old, Jane Smith: 25 years old"
+        regex = "(\w+\s\w+): (\d+) years"
+        self.my_re.search(regex, text)
+        output = self.my_re.grouping()
+        assert(output == ('John Doe', '30'))
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_start(self):
         """Ensure start() works as expected"""
-        debug.trace(4, "test_start()")
-        assert(False)
+        debug.trace(4, f"test_start(); self={self}")
+        text = "three little birds"
+        regex = "\w{5}"
+        self.my_re.search(regex, text)
+        output = self.my_re.start()
+        # start() returns starting index
+        assert(output == 0)
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_end(self):
         """Ensure end() works as expected"""
-        debug.trace(4, "test_end()")
-        assert(False)
+        debug.trace(4, f"test_end(); self={self}")
+        text = "three big birds"
+        regex = "\w{5}"
+        self.my_re.search(regex, text)
+        output = self.my_re.end()
+        # start() returns ending index
+        assert(output == 5)
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_sub(self):
         """Ensure sub() works as expected"""
-        debug.trace(4, "test_sub()")
-        assert(False)
+        debug.trace(4, f"test_sub(); self={self}")
+        text = "The quick brown fox jumps over the lazy dog."
+        regex = "\w{4}"
+        replacement = "****"
+        output_sample = "The ****k ****n fox ****s **** the **** dog."
+        output = self.my_re.sub(pattern=regex, string=text, replacement=replacement)
+        assert(output == output_sample)
 
-    @pytest.mark.xfail                   # TODO: remove xfail
     def test_span(self):
         """Ensure span() works as expected"""
-        debug.trace(4, "test_span()")
-        assert(False)
+        debug.trace(4, f"test_span(); self={self}")
+        text = "The quick brown fox jumps over the lazy dog."
+        regex = "\w{4}"
+        self.my_re.search(regex, text)
+        output = self.my_re.span()
+        assert(output == (4, 8))
+    
+    def test_split(self):
+        """Ensure split() works as expected"""
+        debug.trace(4, f"test_split(); self={self}")
+        text = "three,little,birds"
+        regex = ","
+        output = self.my_re.split(pattern=regex, string=text)
+        assert(output == text.split(","))
+    
+    def test_findall(self):
+        """Ensure findall() works as expected"""
+        debug.trace(4, f"test_findall(); self={self}")
+        text = "There are 32768 possible combinations, with 256 other combinations and 0 impossible combinations."
+        regex = "\d+"
+        output = self.my_re.findall(pattern=regex, string=text)
+        assert(output == ['32768', '256', '0'])
+
+    def test_escape(self):
+        """Ensure escape() works as expected"""
+        debug.trace(4, f"test_escape(); self={self}")
+        text = "foo*bar"
+        output = self.my_re.escape(text)
+        assert("\*" in output)
 
     @pytest.mark.xfail                   # TODO: remove xfail
     @trap_exception
