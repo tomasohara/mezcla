dea47833dc48433d5a5fc1789bba22648a91cc76
Tom O'Hara <tohara@tohara.com>
2024-04-10 22:45:26 -0500

    fixes init_input to honor manual_input

--- mezcla/main.py
+++ mezcla/main.py
@@ -719,10 +719,11 @@ class Main(object):
                     debug.trace_expr(3, self.filename, self.other_filenames)
             else:
                 debug.assertion(isinstance(self.filename, str))
-                debug.assertion(os.path.exists(self.filename))
-                mode = ("r" if (not self.binary_input) else "rb")
-                self.input_stream = system.open_file(self.filename, mode=mode, errors=self.input_error)
-                debug.assertion(self.input_stream)
+                if not self.manual_input:
+                    debug.assertion(os.path.exists(self.filename))
+                    mode = ("r" if (not self.binary_input) else "rb")
+                    self.input_stream = system.open_file(self.filename, mode=mode, errors=self.input_error)
+                    debug.assertion(self.input_stream)
         # Optionally reopen stream to change built-in settings
         error_handling_change = (self.input_error and (self.input_error != self.input_stream.errors))
         reopen_stream = (error_handling_change or self.newlines)
