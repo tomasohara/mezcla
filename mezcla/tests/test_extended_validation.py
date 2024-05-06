f8bbef3a515afa72db9ea61b8f0121610b1fadfa
LimaBD <bdl1998@hotmail.com>
2024-05-06 13:04:35 -0300

    refactor, move function tests into tests

--- mezcla/tests/test_extended_validation.py
+++ mezcla/tests/test_extended_validation.py
@@ -8,15 +8,11 @@ import pytest
 from pydantic import ValidationError, BaseModel, validate_call
 import mezcla.extended_validation as ev
 
-# Functions to test
-
-@validate_call
-@ev.validate_dictionaries()
-def trivial_dict_parameter(some_dict: dict) -> bool:
-    """Example of simple dictionary validation"""
-    assert isinstance(some_dict, dict), "The validation should fail before this"
-    print("@custom_validate_call works!")
-    return True
+def assert_validation_error(func, *args, **kwargs):
+    """Asserts that a function raises a ValidationError"""
+    with pytest.raises(ValidationError) as exc_info:
+        func(*args, **kwargs)
+    assert "For further information visit" in str(exc_info.value)
 
 class ExpectedDictModel(BaseModel):
     """Expected dictionary model"""
@@ -31,26 +27,17 @@ def example_dict_keys_values(some_dict: dict) -> bool:
     print("@custom_validate_call works!")
     return True
 
-class WrongModel:
-    """(Not a Pydantic model child)"""
-    example_key: str
-
-@validate_call
-@ev.validate_dictionaries(some_dict = WrongModel)
-def example_wrong_model(some_dict: dict) -> None:
-    """Example of wrong model passed to custom_validate_call"""
-    raise AssertionError("The validation should fail before this")
-
-# Start of tests
-
-def assert_validation_error(func, *args, **kwargs):
-    """Asserts that a function raises a ValidationError"""
-    with pytest.raises(ValidationError) as exc_info:
-        func(*args, **kwargs)
-    assert "For further information visit" in str(exc_info.value)
-
 def test_trivial_dict_parameter():
     """Test for trivial_dict_parameter"""
+    # Function to test
+    @validate_call
+    @ev.validate_dictionaries()
+    def trivial_dict_parameter(some_dict: dict) -> bool:
+        """Example of simple dictionary validation"""
+        assert isinstance(some_dict, dict), "The validation should fail before this"
+        print("@custom_validate_call works!")
+        return True
+    # Tests
     assert trivial_dict_parameter({})
     assert trivial_dict_parameter({"a": 1, "b": 2})
     assert_validation_error(trivial_dict_parameter, 12345)
@@ -74,11 +61,20 @@ def test_dict_value():
 
 def test_wrong_model():
     """Test for wrong model"""
+    # Class model to test
+    class WrongModel:
+        """(Not a Pydantic model child)"""
+        example_key: str
+    # Function to test
+    @validate_call
+    @ev.validate_dictionaries(some_dict = WrongModel)
+    def example_wrong_model(some_dict: dict) -> None:
+        """Example of wrong model passed to custom_validate_call"""
+        raise AssertionError("The validation should fail before this")
+    # Test
     with pytest.raises(AssertionError) as exc_info:
         example_wrong_model({})
     assert "must be a pydantic.BaseModel class" in str(exc_info.value)
-
-# Run the tests
 
 if __name__ == "__main__":
     pytest.main()
