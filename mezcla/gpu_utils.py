383a1432e7dfabe95b0b5422b09ffea3f944d269
Tom O'Hara <tohara@tohara.com>
2024-05-11 20:10:10 -0500

    adds typehint support; adds GPU_DEBUG_LEVEL for use in trace_gpu_usage, which gets invoked if run as script

--- mezcla/gpu_utils.py
+++ mezcla/gpu_utils.py
@@ -7,7 +7,7 @@
 """GPU support"""
 
 # Standard modules
-## TODO: import numpy
+from typing import Optional
 
 # Installed modules
 import torch
@@ -19,6 +19,7 @@ from mezcla import system
 
 # Constants
 TL = debug.TL
+DEBUG_LEVEL = debug.get_level()
 #
 # Environment-based
 HAS_CUDA = torch.cuda.is_available()
@@ -32,19 +33,28 @@ TORCH_DEVICE = system.getenv_text(
     "TORCH_DEVICE", TORCH_DEVICE_DEFAULT,
     description="Device for running torch"
 )
+GPU_DEBUG_LEVEL = system.getenv_int(
+    "GPU_DEBUG_LEVEL", TL.DETAILED,
+    desc="Default trace level for trace_gpu_usage, etc.")
 
 #-------------------------------------------------------------------------------
 
-def trace_gpu_usage(level=TL.DETAILED):
+def trace_gpu_usage(level: Optional[int] = None):
     """Trace out usage for GPU memory, etc.
+    Note: Optional level defaults to GPU_DEBUG_LEVEL
     TODO: support other types besides NVidia"""
+    if level is None:
+        level = GPU_DEBUG_LEVEL
     if TORCH_DEVICE == "cuda":
         debug.trace(level, "GPU usage")
         debug.trace(level, gh.run("nvidia-smi"))
-
+    ## DEBUG: debug.trace_expr(1, int(level), int(DEBUG_LEVEL), GPU_DEBUG_LEVEL)
+    if level > DEBUG_LEVEL:
+        debug.trace(TL.USUAL, "FYI: Use higher trace level--DEBUG_LEVEL")
 
 #-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     debug.trace_current_context(level=TL.QUITE_VERBOSE)
     system.print_error("Warning: Not intended for direct invocation.")
+    debug.code(TL.USUAL, lambda: trace_gpu_usage())    # pylint: disable=unnecessary-lambda
