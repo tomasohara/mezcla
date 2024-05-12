8d07632b557027bcd55477072d85423d27cb97cd
Tom O'Hara <tohara@tohara.com>
2024-05-11 20:58:00 -0500

    adds show_if_disabed to trace_gpu_usage

--- mezcla/gpu_utils.py
+++ mezcla/gpu_utils.py
@@ -55,6 +55,7 @@ def trace_gpu_usage(level: Optional[int] = None, show_if_disabed: Optional[bool]
     ## DEBUG: debug.trace_expr(1, int(level), int(DEBUG_LEVEL), GPU_DEBUG_LEVEL)
     if level > DEBUG_LEVEL:
         debug.trace(TL.USUAL, "FYI: Use higher trace level--DEBUG_LEVEL")
+ 
 
 #-------------------------------------------------------------------------------
         
