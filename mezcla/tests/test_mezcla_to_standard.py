ed7ee33c66ae75840dd79ae77945945c90d076fc
Aviyan Acharya <avyn.xyz@gmail.com>
2024-05-30 21:09:15 +0545

    Added unittest for methods of mezcla_to_standard: _insert_extra_params, run_target, run_dest, use_standard_equivalent, use_mezcla_equivalent, insert_decorator_to_functions

--- mezcla/tests/test_mezcla_to_standard.py
+++ mezcla/tests/test_mezcla_to_standard.py
@@ -246,6 +246,217 @@ class TestIt(TestWrapper):
         result = eqcall._to_dest_args_keys(args)
         assert result == expected_result
 
+    # XPASS
+    @pytest.mark.xfail
+    def test_eqcall__insert_extra_params(self):
+        """Make sure that _insert_extra_paramas of EqCall class works as usual"""
+        
+        # Creating mock functions for testing        
+        def slope(x1:int, y1:int, x2:int, y2:int):
+            return (y2 - y1)/(x2 - x1)
+        
+        extra_params = {"x1": 3, "y1": 5, "x2": 6}
+        args = {"x1": 10, "y2": 12}
+
+        # Union of dictionary
+        expected_result = extra_params|args
+
+        eqcall = THE_MODULE.EqCall(target=slope, dest=None, extra_params=extra_params)
+        result = eqcall._insert_extra_params(args)
+        assert result == expected_result
+
+    # XPASS
+    @pytest.mark.xfail
+    def test_eqcall_run_target(self):
+        """Make sure that run_target of EqCall class works as usual"""
+        def slope(x1:int, y1:int, x2:int, y2:int) -> float:
+            return round((y2 - y1)/(x2 - x1), 2)
+        
+        def distance(x1:int, y1:int, x2:int, y2:int) -> float:
+            return round(( (x2 - x1) ** 2 + (y2 - y1) ** 2 ) ** 0.5, 2)
+        
+        args = {"x1": 4, "y1": 5, "x2": 6, "y2": 10}
+        expected_result = 2.5
+
+        eqcall = THE_MODULE.EqCall(target=slope, dest=distance)
+        result = eqcall.run_target(**args)
+        assert result == expected_result
+
+    # XPASS
+    @pytest.mark.xfail
+    def test_eqcall_run_dest(self):
+        """Make sure that run_dest of EqCall class works as usual"""
+        def slope(x1:int, y1:int, x2:int, y2:int) -> float:
+            return round((y2 - y1)/(x2 - x1), 2)
+        
+        def distance(x1:int, y1:int, x2:int, y2:int) -> float:
+            return round(( (x2 - x1) ** 2 + (y2 - y1) ** 2 ) ** 0.5, 2)
+        
+        args = {"x1": 4, "y1": 5, "x2": 6, "y2": 10}
+        expected_result = 5.39
+
+        eqcall = THE_MODULE.EqCall(target=slope, dest=distance)
+        result = eqcall.run_dest(**args)
+        assert result == expected_result
+
+    # XPASS
+    @pytest.mark.xfail
+    def test_use_standard_equivalent(self):
+        """Make sure that use_standard_equivalent works as usual"""
+
+        class MockCall:
+            def __init__(self, target_func, dest_func, condition_met) -> None:
+                self.target_func = target_func
+                self.dest_func = dest_func
+                self.condition_met = condition_met
+
+            def same_target(self, func):
+                return self.target_func == func
+            
+            def is_dest_condition_met(self, *args, **kwargs):
+                return self.condition_met(*args, **kwargs)
+            
+            def run_dest(self, *args, **kwargs):
+                return self.dest_func(*args, **kwargs)
+            
+        def mock_target_func(*args, **kwargs):
+            return "target"
+        
+        def mock_dest_func(*args, **kwargs):
+            return "destination"
+        
+        def always_true(*args, **kwargs):
+            return True
+
+        def always_false(*args, **kwargs):
+            return False
+        
+        # Mock mezcla_to_standard to verify mock_target_func
+        THE_MODULE.mezcla_to_standard = [
+            MockCall(target_func=mock_target_func, condition_met=always_true, dest_func=mock_dest_func),
+            MockCall(target_func=mock_target_func, condition_met=always_false, dest_func=mock_dest_func)
+        ]
+
+        # Apply the decorator
+        decorated_func = THE_MODULE.use_standard_equivalent(mock_target_func)
+        assert decorated_func() == "destination"
+
+        # Re-mock mezcla_to_standard to verify mock_dest_func
+        THE_MODULE.mezcla_to_standard = [
+            MockCall(target_func=mock_dest_func, condition_met=always_false, dest_func=mock_target_func),
+            MockCall(target_func=mock_dest_func, condition_met=always_true, dest_func=mock_target_func)
+        ]
+
+        decorated_func = THE_MODULE.use_standard_equivalent(mock_dest_func)
+        assert decorated_func() == "target"
+
+    # XPASS
+    @pytest.mark.xfail
+    def test_use_mezcla_equivalent(self):
+        """Make sure that use_mezcla_equivalent works as usual"""
+
+        class MockCall:
+            def __init__(self, dest_func, target_func, condition_met) -> None:
+                self.dest_func = dest_func
+                self.target_func = target_func
+                self.condition_met = condition_met
+
+            def same_dest(self, func):
+                return self.dest_func == func
+
+            def is_target_condition_met(self, *args, **kwargs):
+                return self.condition_met(*args, **kwargs)
+
+            def run_target(self, *args, **kwargs):
+                return self.target_func(*args, **kwargs)
+
+        def mock_dest_func(*args, **kwargs):
+            return "destination"
+
+        def mock_target_func(*args, **kwargs):
+            return "target"
+
+        def always_true(*args, **kwargs):
+            return True
+
+        def always_false(*args, **kwargs):
+            return False
+
+        # Mock mezcla_to_standard in THE_MODULE
+        THE_MODULE.mezcla_to_standard = [
+            MockCall(dest_func=mock_dest_func, target_func=mock_target_func, condition_met=always_true),
+            MockCall(dest_func=mock_dest_func, target_func=mock_target_func, condition_met=always_false)
+        ]
+
+        # Apply the decorator
+        decorated_func = THE_MODULE.use_mezcla_equivalent(mock_dest_func)
+        assert decorated_func() == "target"
+
+        # Re-mock mezcla_to_standard to verify mock_target_func
+        THE_MODULE.mezcla_to_standard = [
+            MockCall(dest_func=mock_target_func, target_func=mock_dest_func, condition_met=always_true),
+            MockCall(dest_func=mock_target_func, target_func=mock_dest_func, condition_met=always_false)
+        ]
+
+        decorated_func = THE_MODULE.use_mezcla_equivalent(mock_target_func)
+        assert decorated_func() == "destination"
+
+    # XPASS
+    @pytest.mark.xfail
+    def test_insert_decorator_to_functions(self):
+        """Make sure that insert_decorator_to_functions works as expected"""
+
+        def sample_decorator(func):
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+            return wrapper
+
+        original_code = '''
+def foo():
+    print("Hello, World!")
+    
+def bar(x):
+    return x * 2
+'''
+
+        # Expected modified code (uses the format of the decorator)
+        _expected_code = '''
+from mezcla.mezcla_to_standard import sample_decorator
+
+@sample_decorator
+def foo():
+    print("Hello, World!")
+    
+@sample_decorator
+def bar(x):
+    return x * 2
+'''         
+        # Actually the sample decorator is wrapped arround print
+        # Within the test code, avoid double quotes
+        expected_code = '''
+from mezcla.mezcla_to_standard import sample_decorator
+
+def foo():
+    sample_decorator(print)('Hello, World!')
+
+def bar(x):
+    return x * 2
+'''
+            # Run the function to insert the decorator
+        result = THE_MODULE.insert_decorator_to_functions(sample_decorator, original_code)
+        
+        # Normalize whitespace for comparison
+        def normalize_whitespace(s):
+            return ''.join(s.split())
+        
+        normalized_result = normalize_whitespace(result)
+        normalized_expected_code = normalize_whitespace(expected_code)
+        
+        # Assert that the result matches the expected code
+        assert repr(normalized_result) == repr(normalized_expected_code)
+
+
+
 if __name__ == '__main__':
     debug.trace_current_context()
     pytest.main([__file__])
