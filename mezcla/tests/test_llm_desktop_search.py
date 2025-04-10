d015c85b5d403ffe849ec307b5ff267c3565f591
Tom O'Hara <tohara@tohara.com>
2025-04-10 02:16:24 -0500

    fixes skipif spec; code cleanup

--- mezcla/tests/test_llm_desktop_search.py
+++ mezcla/tests/test_llm_desktop_search.py
@@ -27,24 +27,22 @@
 
 # Standard modules
 import atexit
-## OLD: from collections.abc import Iterable
 
 # Installed modules
 import pytest
 
 # Local modules
-from mezcla.unittest_wrapper import TestWrapper, RUN_SLOW_TESTS
-from mezcla.unittest_wrapper import trap_exception, invoke_tests
+from mezcla.unittest_wrapper import TestWrapper, RUN_SLOW_TESTS, invoke_tests
 from mezcla import debug
 from mezcla import glue_helpers as gh
-from  mezcla import gpu_utils
+from mezcla import gpu_utils
 from mezcla.main import KEEP_TEMP_FILES
 from mezcla.my_regex import my_re
 from mezcla import system
 
 # Note: Two references are used for the module to be tested:
-#    THE_MODULE:                        global module object
-#    TestIt.script_module:              path to file
+#    THE_MODULE:                        module instance (e.g,, <module 'mezcla.main' from '/home/testuser/Mezcla/mezcla/main.py'>
+#    TestIt.script_module:              dotted module path (e.g., "mezcla.main")
 import mezcla.llm_desktop_search as THE_MODULE
 get_last_modified_date = THE_MODULE.get_last_modified_date
 
@@ -58,7 +56,6 @@ class TestIt(TestWrapper):
     
     # set a temp dir to test index indexingL setUpClass
     # Note: index_temp_dir needs to be unique
-    ## OLD: index_temp_dir = gh.form_path(system.TEMP_DIR, INDEX_STORE_DIR)
     index_temp_dir = None
     index_parent = None
 
@@ -80,7 +77,6 @@ class TestIt(TestWrapper):
 
     @pytest.mark.xfail                   # TODO: remove xfail
     @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="Ignoring slow test")
-    ## DEBUG: @trap_exception            # TODO: remove when debugged
     def test_01_index_dir(self):
         """Tests run_script to index directory"""
         # Warning: see notes above about potential issues with run_script-based tests.
@@ -95,13 +91,9 @@ class TestIt(TestWrapper):
         # test if indexing works with with no existing db
         file_dir = gh.real_path(gh.dirname(__file__))
         repo_base_dir = gh.form_path(file_dir, "..", "..")
-        ## OLD: index_file = self.get_temp_file()
         
         self.run_script(options=f"--index {repo_base_dir}",
-                        env_options=f"INDEX_STORE_DIR={self.index_temp_dir}")
-        ## OLD: self.run_script(options=f"--index {repo_base_dir}",
-        #                       env_options=f"INDEX_STORE_DIR={self.index_temp_dir}")
-        
+                        env_options=f"INDEX_STORE_DIR={self.index_temp_dir}")        
         index_files = system.read_directory(self.index_temp_dir)
         
         # assert INDEX_STORE_DIR is not empty
@@ -121,9 +113,8 @@ class TestIt(TestWrapper):
         self.do_assert(new_date > prev_date)
         
 
-    @pytest.mark.skipif(RUN_SLOW_TESTS, reason="Ignoring slow test")
+    @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="Ignoring slow test")
     @pytest.mark.xfail                   # TODO: remove xfail
-    ## DEBUG: @trap_exception            # TODO: remove when debugged
     def test_02_search_docs(self):
         """Test for something_else: TODO..."""
         debug.trace(4, f"TestIt.test_02_search_docs(); self={self}")
@@ -140,9 +131,9 @@ class TestIt(TestWrapper):
     def test_03_gpu_usage(self):
         """Test for GPU libs being used"""
         # Note: verifies process using GPU via nvida-smi
-        #  |  GPU   GI   CI        PID   Type   Process name                            GPU Memory |
+        #  |  GPU   GI   CI        PID   Type   Process name            GPU Memory |
         #  ...
-        #  |    0   N/A  N/A   1111609      C   python                                      366MiB |
+        #  |    0   N/A  N/A   1111609      C   python                      366MiB |
         debug.trace(4, f"TestIt.test_03_gpu_usage(); self={self}")
         ds = THE_MODULE.DesktopSearch(self.index_temp_dir)
         ds.show_similar("license")
@@ -150,7 +141,6 @@ class TestIt(TestWrapper):
         gpu_utils.trace_gpu_usage(level=trace_level)
         stdout, stderr = self.get_stdout_stderr()
         self.do_assert("GNU" in stdout) 
-        ## OLD: self.do_assert("tensorflow" in stderr)
         pid = system.get_process_id()
         self.do_assert(my_re.search(fr"\b{pid}\b.*python", stderr))
         return
@@ -172,7 +162,6 @@ class TestIt(TestWrapper):
 
     @pytest.mark.xfail                   # TODO: remove xfail
     @pytest.mark.skipif(not RUN_SLOW_TESTS, reason="Ignoring slow test")
-    @trap_exception                      # TODO: remove when debugged
     def test_05_index_via_API(self):
         """Run indexing via class-based API"""
         debug.trace(4, f"test_05_index_via_API(): self={self}")
