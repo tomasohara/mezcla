1e319d31cb6c11e04660dc379ceb8f999faeaf9d
Tom O'Hara <tohara@tohara.com>
2025-04-09 21:14:22 -0500

    imports HOME [with ~ expanded]; makes USING_GITHUB_RUNNER an env option; disables tracing in setUp for sake of capsys; uses get_stderr for capsys; misc. cleanup

--- mezcla/tfidf/tests/misc_test.py
+++ mezcla/tfidf/tests/misc_test.py
@@ -14,6 +14,7 @@ import pytest
 # Local packages
 from mezcla import debug
 from mezcla import system
+from mezcla import glue_helpers as gh
 from mezcla.unittest_wrapper import TestWrapper
 from mezcla.unittest_wrapper import trap_exception
 from mezcla.tfidf.corpus import Corpus
@@ -32,20 +33,32 @@ TOP_BIGRAMS =  ["",  "1 2",  "2 4",  "3 4",  ""]
 TOP_UNIGRAMS = ["0",  "1",    "2",    "3",    ""]
 
 # Environment settings
-HOME = system.getenv_text(
-    "HOME", "~",
-    description="Home directory for user")
-USING_GITHUB_RUNNER = ("/home/runner" in HOME)
+## OLD:
+## HOME = system.getenv_text(
+##     "HOME", "~",
+##     description="Home directory for user")
+## USING_GITHUB_RUNNER = ("/home/runner" in HOME)
+USING_GITHUB_RUNNER_DEFAULT = ("/home/runner" in gh.HOME_DIR)
+USING_GITHUB_RUNNER = system.getenv_bool(
+    "USING_GITHUB_RUNNER", USING_GITHUB_RUNNER_DEFAULT,
+    desc="Whether likely using the Github Actions runner VM")
 
 class TestMisc(TestWrapper):
     """Class for test case definitions"""
     script_module= None
     use_temp_base_dir = True
 
-    @pytest.fixture(autouse=True)
-    def capsys(self, capsys):
-        """Gets capsys"""
-        self.capsys = capsys
+    def setUp(self):
+        """Performs test setup with capsys disabled
+           Note: This works around quirk with pytest stderr capturing"""
+        with self.capsys.disabled():
+            super().setUp()
+    
+    ## OLD:
+    ## @pytest.fixture(autouse=True)
+    ## def capsys(self, capsys):
+    ##     """Gets capsys"""
+    ##     self.capsys = capsys
 
     @pytest.mark.xfail                   # TODO: remove xfail
     @trap_exception
@@ -80,29 +93,34 @@ class TestMisc(TestWrapper):
             self.do_assert(is_top_term(TOP_BIGRAMS[d], bi_corpus.get_keywords(docid)))
         debug.trace(5, "out test_01_simple_tfidf()")
 
+    @pytest.mark.xfail                   # TODO: remove xfail
     @pytest.mark.skipif(not USING_GITHUB_RUNNER,
                         reason="The following is only for the Github runner VM")
     def test_02_no_dockeyword_tracing(self):
         """Make sure no debug calls in DocKeyword class
            Note: This is intended as a sanity check to ensure the debug calls aren't
-           checked in by accident. That it, the '## DEBUG:' prefix should only be
+           checked in by accident. That is, the '## DEBUG:' prefix should only be
            removed in a local version."""
         # Set the highest-possible debugging level
         ## TODO2: use monkey patch
         save_debug_level = debug.get_level()
         debug.set_level(system.MAX_INT)
-        # note: make sure stderr empty by capturing it before initializer (DocKeyword)
-        pre_init_stderr_text = self.capsys.readouterr().err
+        # note: make sure stderr empty by capturing it before initializer (DocKeyword).
+        # In addition, the setup method ensures that capsys is disabed beforehand.
+        ## OLD: pre_init_stderr_text = self.capsys.readouterr().err
+        self.clear_stdout_stderr()
+        pre_init_stderr_text = self.get_stderr()
 
         # Perform the test proper
         dummy_word = "dummy"
         dk = DocKeyword(dummy_word)
-        init_stderr_text = self.capsys.readouterr().err
-        self.do_assert(not init_stderr_text)
+        ## OLD: init_stderr_text = self.capsys.readouterr().err
+        init_stderr_text = self.get_stderr()
+        debug.trace_expr(5, pre_init_stderr_text, init_stderr_text, dk)
+        self.do_assert(not init_stderr_text.strip())
         self.do_assert(dk.text == dummy_word)
 
         # Post-test sanity checks
         debug.assertion(isinstance(pre_init_stderr_text, str))
         debug.assertion(isinstance(init_stderr_text, str))
         debug.set_level(save_debug_level)
-        debug.trace_expr(5, pre_init_stderr_text, init_stderr_text, dk)
