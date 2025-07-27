763d86e503f36bd1126f847e1dabf3e55feccc0d
Scrappy Cito <scrappycito@gmail.com>
2025-07-27 18:40:52 -0500

    adds test_format_checkbox and refines test_format_input_field

--- mezcla/tests/test_html_utils.py
+++ mezcla/tests/test_html_utils.py
@@ -643,18 +643,28 @@ class TestHtmlUtils(TestWrapper):
         assert result is None  
 
     @pytest.mark.xfail
+    def test_format_checkbox(self):
+        """Verify simple format_checkbox usage"""
+        # ex: <input type='hidden' name='fubar' value='off'><label >Fubar?<input type='checkbox' id='fubar-id' name='fubar'   ></label>"
+        field_spec = THE_MODULE.format_checkbox("fubar")
+        assert my_re.search(r"<input type='hidden'\s*name='fubar'\s*value='off'><label\s*>Fubar\?&nbsp;<input\s*type='checkbox'\s*id='fubar-id'\s*name='fubar'\s*></label>",
+                            field_spec)
+        field_spec = THE_MODULE.format_checkbox("fubar", disabled=True, concat_label=True)
+        assert my_re.search(r"<label\s*>Fubar\?<input\s*type='checkbox'\s*id='fubar-id'\s*name='fubar'\s*disabled\s*></label>",
+                            field_spec)
+
+    @pytest.mark.xfail
     def test_format_input_field(self):
-        """Verify format_input_field"""
+        """Verify simple format_input_field usage"""
         ## HACK: ensures that single quotes used in tested result
         #
-        field_spec = THE_MODULE.format_input_field("name", label="Name:").replace('"', "'")
-        assert my_re.search(r"<label\s*>Name:&nbsp;<input id='name-id'\s*name='name'\s*></label>",
+        field_spec = THE_MODULE.format_input_field("first", label="First:").replace('"', "'")
+        assert my_re.search(r"<label\s*>First:&nbsp;<input id='first-id'\s*name='first'\s*></label>",
                             field_spec)
         #
-        field_spec = THE_MODULE.format_input_field("name", label="Name:", default_value="Joe").replace('"', "'")
-        assert my_re.search(r"<label\s*>Name:&nbsp;<input id='name-id'\s*value='Joe' name='name'\s*></label>",
+        field_spec = THE_MODULE.format_input_field("last", label="Last:", default_value="Doe").replace('"', "'")
+        assert my_re.search(r"<label\s*>Last:&nbsp;<input id='last-id'\s*value='Doe' name='last'\s*></label>",
                             field_spec)
-                            
         #
         field_spec = THE_MODULE.format_input_field("age", label="Age:", field_type="number", default_value="19").replace('"', "'")
         assert my_re.search(r"<label\s*>Age:&nbsp;<input id='age-id'\s*value='19'\s*name='age'\s*type='number'\s*></label>",
