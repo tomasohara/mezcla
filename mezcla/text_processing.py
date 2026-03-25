b85f5befcdbf5881e900b2f527f8dc4cf7847f9e
Tom O'Hara <tohara@tohara.com>
2026-03-24 22:42:07 -0400

    uses hardcoded stopwords if NLTK not available

--- mezcla/text_processing.py
+++ mezcla/text_processing.py
@@ -345,10 +345,14 @@ def is_stopword(word):
     # note: Intended as a quick filter for excluding non-content words.
     global stopwords
     if (stopwords is None):
-        if SKIP_NLTK:
+        stopwords = []
+        if not SKIP_NLTK:
+            try:
+                stopwords = nltk.corpus.stopwords.words('english')
+            except:
+                system.print_exception_info("loading stopwords")
+        if not stopwords:
             stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
-        else:
-            stopwords = nltk.corpus.stopwords.words('english')
         debug.trace(6, "stopwords: %s" % stopwords)
     return (word.lower() in stopwords)
 
